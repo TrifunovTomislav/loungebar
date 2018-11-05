3d4cf827180b1b2f44d94abf581d579a8d837502
Trifunov Tomilsav <trifunovt@gmail.com>
2018-11-05 12:30:27 +0100

    done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://administrator1:administrator1@ds133533.mlab.com:33533/devloungebar"
+    "mongodb://administrator1:administrator1@ds133533.mlab.com:33533/devloungebar",
+  secretOrKey: "secret"
 };
